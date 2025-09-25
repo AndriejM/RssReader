2131b2b29cb50c40d3991c24a9c632b100a12807
AndriejSoft <andriejsoft@gmail.com>
2025-09-25 19:44:56 +0200

    RSS reader initialisation.

---
+++ src/test/java/pl/andriejsoft/rssreader/RssreaderApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.andriejsoft.rssreader;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RssreaderApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
